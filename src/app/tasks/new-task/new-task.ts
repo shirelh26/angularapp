922ec8bd18062e400ba42e758e871dd8373d9b6d
חסון שיראל <2880216@main.local>
2025-11-16 16:29:22 +0200

    new task error

---
+++ src/app/tasks/new-task/new-task.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule,NgForm,NgModel } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-task',
+  imports: [FormsModule],
+  templateUrl: './new-task.html',
+  styleUrl: './new-task.css'
+})
+export class NewTask {
+title='Title';
+summary='Summary';
+dueDate='';
+onSubmit(form:NgForm){
+  console.log(this.title);
+  console.log(form);
+}
+}
--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Input } from '@angular/core';
 import { Task } from './task/task';
 import { fakeTasks } from './fake_tasks'; 
+import { NewTask } from './new-task/new-task';
 
 @Component({
   selector: 'app-tasks',
-  imports: [Task],
+  imports: [Task,NewTask],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css'
 })
@@ -13,7 +14,7 @@ export class Tasks {
    tasks=fakeTasks;
  @Input() name?: string;
   @Input() userId?: string;
-
+showNewTask = false;
 
 
  get userSelectedTasks() {
@@ -22,4 +23,12 @@ return this.tasks.filter((task) => task.userId === this.userId)
 trackById(index: number, item: any) {
     return item.id;
   }
+  showNew(){
+    this.showNewTask=true;
+  
+  }
+  onTaskCompleted(taskId:string){
+    this.tasks=this.tasks.filter((task)=>task.id!==taskId)
+  }
+  
 }
